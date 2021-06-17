a7d7b34ce2f9dc7664b273e1699a3e8fc8e43620
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2021-06-17 22:20:52 +0600

    236A

---
+++ 236A/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+string a;
+int pas = 0, num;
+int main() {
+    cin >> a;
+    for (int i = 0; i < a.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[i] == a[j]) {
+                num++;
+            }
+        }
+        if (num == 0) {
+            pas++;
+        }
+        num = 0;
+    }
+    cout<<pas;
+
+    if (pas % 2 == 0) {
+         cout << "CHAT WITH HER!";
+    } else {
+
+         cout << "IGNORE HIM!";
+    }
+}
---
+++ 4A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int w;
+
+int main() {
+    cin >> w;
+    (w>2 && w%2==0)? cout<<"YES":cout<<"NO";
+}
